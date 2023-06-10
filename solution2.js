ebc04001507fd07db85050e093111e07f2301add
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-06-10 11:07:13 +0530

    added solution2 file

---
+++ solution2.js
@@ -0,0 +1,18 @@
+//Write a program that checks for a prime number
+
+let a = 1;
+if(a%2==0){
+    console.log("It is a not Prime number")   
+}
+else if(a%3==0){
+    console.log("It is a not Prime number")
+}
+else if(a%5==0){
+    console.log("It is a not Prime number")
+}
+else if(a==1){
+    console.log("It is a not Prime number")
+}
+else{
+    console.log("It is a Prime number")
+}
