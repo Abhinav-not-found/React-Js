b5e4459a39bdfca276bcee9ecf3e1b0f8308b621
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-06-10 10:53:32 +0530

    added new file

---
+++ assignment1.js
@@ -0,0 +1,37 @@
+//Write a program to print numbers from 1 to 20
+// for(let i=1;i<=20;i++){
+//     console.log(i);
+// }
+//Write a program that checks for a prime number
+
+let a = 1;
+if(a%2==0){
+    console.log("It is a not Prime number")   
+}
+else if(a%3==0){
+    console.log("It is a not Prime number")
+}
+else if(a%5==0){
+    console.log("It is a not Prime number")
+}
+else if(a==1){
+    console.log("It is a not Prime number")
+}
+else{
+    console.log("It is a Prime number")
+}
+
+
+
+
+
+
+
+// write a program that calculate the sum from 1 to 100
+    // let i=1,sum=0;
+    // do{
+    //     sum+=i;
+    //     i++;
+    // }
+    // while(i<=100);
+    // console.log(sum)
