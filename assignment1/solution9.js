29f13fb60d0b3fa227e965bcd08e82516b805032
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-06-13 11:34:53 +0530

    updated

--- assignment1/solution9.js
+++ assignment1/solution9.js
@@ -1,7 +1,7 @@
 //Write an annonymous funciton that takes an array of numbers as an argument and return
 //the largest number in the array.
 let array = function(arr){
-    let max=0;
+    let max=arr[0];
     for(let i=0;i<arr.length;i++){
         if(arr[i]>max){
             max=arr[i];
@@ -9,4 +9,5 @@ let array = function(arr){
     }
     return "Largest number = " + max;
 }
-console.log(array("1,2,3,4,5"))
+console.log(array([-5,6,-99,205]))
+//yes sir
