72d9eb20a09a743185699ff39477630b4d1bbc2d
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-06-20 11:48:50 +0530

    updated

--- assignment1/solution16.js
+++ assignment1/solution16.js
@@ -5,29 +5,33 @@
 //Create an instance of the student class and add multiple subjects with their respective grades to the subject property. Call the 
 //getAverageGrade() method and print the average grade.
 
-
+//Create an instance of the student class and add multiple subjects. Use the addSubject() method to add a new subject and its grade. Call the 
+//getAverageGrade() method and print the updated average grade.
 
 
 class Student{
-    constructor(name,age,subject=[name,grade]){
+    constructor(name,age,subject){
         this.name=name;
         this.age=age;
         this.subject=subject;
     }
-    
-    getAverageGrade(grade) {
-      this.grade=grade
-        let sum=0;
+    getAverageGrade(){
+        let sum=0,avg=0;
         for(let i=0;i<this.subject.length;i++){
-            sum=sum+ this.subject[i][1]
-        }  
-        let avg=sum/4;
+            sum=sum+this.subject[i].grade
+        }
+        avg=sum/this.subject.length;
         console.log(avg)
-
+    }
+    addSubject(subject){
+      this.subject.push(subject)
+      console.log(this.subject)
+      
     }
 }
-let s1=new Student("abhinav",22,[["math",21],["english",25],["physics",24],["chemistry",28]],)
-let s2=new Student("adarsh",21,[["math",29],["english",28],["physics",29],["chemistry",30]],)
-
+let s1=new Student("abhinav",22,[{name:"math",grade:28},{name:"english",grade:27},{name:"physics",grade:26}])
+s1.addSubject({
+    name:"chemistry",
+    grade:30
+});
 s1.getAverageGrade()
-s2.getAverageGrade()
